41253894ffb78cf6c97bc20411f3036b89b6952f
Margaux Bonet <bonet.margaux@gmail.com>
2023-03-29 23:00:41 +0200

    Update messages & replies

--- src/views/messages.php
+++ src/views/messages.php
@@ -13,70 +13,69 @@
 (<?php echo $topic_title; ?>)</h1>
 
 
-<?php
-// Récupération des messages
-$messages = $postModel->getMessagesBySubtopicId($subtopic_id);
-
-if (empty($messages)) {
-    echo "Aucun message pour le moment.";
-} else {
-    // Affichage des messages et des réponses
-    foreach ($messages as $message) {
-        // Affichage du message
-        ?>
-        <div>
-            <h3> Message </h3>
-            <p> <?= $message['message'] ?> </p>
-            <p> Message posté par <?= $message['username'] ?> le <?= strftime("%d/%m/%Y à %Hh%M", strtotime($message['created_at'])) ?> </p>
-
-
-
-            <!-- Formulaire pour ajouter une réponse au message d'origine -->
-            <div style='margin-left: 20px;'>
-                <h3> Réponses </h3>
-                <form method='post' action='index.php?action=addReply'>
-                    <input type='hidden' name='message_id' value='<?= $message['message_id'] ?>'>
-                    <input type='hidden' name='subtopic_id' value='<?= $subtopic_id ?>'>
-                    <textarea name='content'></textarea><br>
-                    <input type='submit' value='Répondre'>
-                </form>
-            </div>
-
-            <?php
-            // Récupération des réponses associées au message
-            $replies = $postModel->getReplies($message['message_id']);
-
-            if (!empty($replies)) {
-                // Affichage des réponses
-                foreach ($replies as $reply) {
-                    ?>
-                    <div style='margin-left: 20px;'>
-                        <p> <?= $reply['content'] ?> </p>
-                        <p> Réponse postée par <?= $reply['username'] ?> le <?= strftime("%d/%m/%Y à %Hh%M", strtotime($reply['created_at'])) ?> </p>
-
-                        <!-- Formulaire pour ajouter une réponse à une réponse existante -->
-                        <div style='margin-left: 20px;'>
-                            <form method='post' action='index.php?action=addReply'>
-                                <input type='hidden' name='message_id' value='<?= $message['message_id'] ?>'>
-                                <input type='hidden' name='subtopic_id' value='<?= $subtopic_id ?>'>
-                                <input type='hidden' name='parent_reply_id' value='<?= $reply['reply_id'] ?>'>
-                                <textarea name='content'></textarea><br>
-                                <input type='submit' value='Répondre'>
-                            </form>
-                        </div>
-                    </div>
-                    <?php
-                }
-            }
-        ?>
-        </div>
+<h1>Messages</h1>
         <?php
-    }
-}
-?>
-
-
-
+        // Récupération des messages
+        $messages = $postModel->getMessagesBySubtopicId($subtopic_id);
+        
+        if (empty($messages)) {
+            echo "Aucun message pour le moment.";
+        } else {
+            // Affichage des messages et des réponses
+            foreach ($messages as $message) {
+                ?>
+                <div class="message">
+                    <h3 class="message-title"> Message </h3>
+                    <div class="message-content">
+                        <p> <?= $message['message'] ?> </p>
+                        <p> Message posté par <?= $message['username'] ?> le <?= strftime("%d/%m/%Y à %Hh%M", strtotime($message['created_at'])) ?> </p>
+                    </div>
+        
+                    <!-- Formulaire pour ajouter une réponse au message d'origine -->
+                    <div class="reply-form">
+                        <form method="post" action="index.php?action=addReply">
+                            <input type="hidden" name="message_id" value="<?= $message['message_id'] ?>">
+                            <input type="hidden" name="subtopic_id" value="<?= $subtopic_id ?>">
+                            <textarea name="content"></textarea><br>
+                            <input type="submit" value="Répondre">
+                        </form>
+                    </div>
+        
+                    <?php
+                    // Récupération des réponses associées au message
+                    $replies = $postModel->getReplies($message['message_id']);
+        
+                    if (!empty($replies)) {
+                        // Affichage des réponses
+                        foreach ($replies as $reply) {
+                            ?>
+                            <div class="reply">
+                                <div class="reply-content">
+                                    <p> <?= $reply['content'] ?> </p>
+                                    <p> Réponse postée par <?= $reply['username'] ?> le <?= strftime("%d/%m/%Y à %Hh%M", strtotime($reply['created_at'])) ?> </p>
+                                </div>
+        
+                                <!-- Formulaire pour ajouter une réponse à une réponse existante -->
+                                <div class="reply-form">
+                                    <form method="post" action="index.php?action=addReply">
+                                        <input type="hidden" name="message_id" value="<?= $message['message_id'] ?>">
+                                        <input type="hidden" name="subtopic_id" value="<?= $subtopic_id ?>">
+                                        <input type="hidden" name="parent_reply_id" value="<?= $reply['reply_id'] ?>">
+                                        <textarea name="content"></textarea><br>
+                                        <input type="submit" value="Répondre">
+                                    </form>
+                                </div>
+                            </div>
+                            <?php
+                        }
+                    }
+                ?>
+                </div>
+                <?php
+            }
+        }
+        ?>
+        
 
 <h2>Ajouter un message</h2>
 <div class="container">
@@ -89,7 +88,6 @@ if (empty($messages)) {
 
 
 <a href="index.php?action=subtopic">Retour</a>
-
 
 
 </body>
